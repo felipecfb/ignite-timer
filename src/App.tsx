1a1a194175ef8866cd3c734221f6660ab44b1b83
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-10-04 12:08:17 -0300

    ✨ feat: adding and configuring eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
@@ -11,5 +11,5 @@ export function App() {
         <GlobalStyle />
       </ThemeProvider>
     </>
-  );
+  )
 }
